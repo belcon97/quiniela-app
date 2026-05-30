19cf6cf9b70aa09f8b773c34dd5bc67b943f3d25
Andres Belandria <andresbelcon@gmail.com>
2026-05-30 13:27:03 -0300

    FIX-- agregar RequestHandler type en rankingController para resolver error de build

--- backend/src/controllers/rankingController.ts
+++ backend/src/controllers/rankingController.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response, RequestHandler } from "express";
 import { getRankingService } from "../services/rankingService";
 
-export const getRanking = async (req: Request, res: Response) => {
+export const getRanking: RequestHandler = async (req: Request, res: Response) => {
   try {
     const ranking = await getRankingService();
     res.status(200).json(ranking);
