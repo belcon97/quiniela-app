2b4e3207f1bd45e4e8cf53e5d0ce3f6b01a5b45c
Andres Belandria <andresbelcon@gmail.com>
2026-06-16 10:42:13 -0300

    FIX-- los tokens se quedaran activos por 30d

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -3,6 +3,6 @@ import jwt from "jsonwebtoken";
 // Genera un token
 export const generateToken = (payload: { id: string; role: string }) => {
   return jwt.sign(payload, process.env.JWT_SECRET as string, {
-    expiresIn: "7d",
+    expiresIn: "30d",
   });
 };
